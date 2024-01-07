7030af64511b16b02dd6c77140a31206843df3f8
Felippe Andrade <4drade@gmail.com>
2024-01-07 17:24:33 -0300

    Refactor code for better readability and maintainability

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
-from string import Template
 import re
+from string import Template
+
 from botasaurus import AntiDetectDriver
 
 # region Selectors
@@ -15,7 +16,7 @@ selectors = {
     },
     "message": "span[class='_11JPr selectable-text copyable-text']",
     "hour": "span[class='l7jjieqr fewfhwl7'][dir='auto']",
-    "reactions": "button[class='dhq51u3o']"
+    "reactions": "button[class='dhq51u3o']",
 }
 # endregion
 
@@ -33,15 +34,18 @@ def extract_list(list_elements: list) -> list[str]:
     return [pattern.sub(" ", element.text.strip()) for element in list_elements]
 
 
-def get_content(driver: AntiDetectDriver) -> list[str]:
+def get_messages(driver: AntiDetectDriver) -> list[str]:
     """
-    Get the content of the messages in the AntiDetectDriver.
+    Get the content of the messages in the Whatsapp Channel.
 
+    :param min_length: The minimum length of the message.
     :param driver: The AntiDetectDriver object representing the web driver.
     :return: A list of the messages in the driver.
     """
-    raw_messages = extract_list(driver.get_elements_or_none_by_selector(selectors['message']))
-    return [re.split(r'[.!?]', message)[0] for message in raw_messages]
+    raw_messages = extract_list(
+        driver.get_elements_or_none_by_selector(selectors["message"])
+    )
+    return [re.split(r"[.!?]", message)[0] for message in raw_messages]
 
 
 def get_hour(driver: AntiDetectDriver) -> list[str]:
@@ -51,12 +55,14 @@ def get_hour(driver: AntiDetectDriver) -> list[str]:
     :param driver: The driver used to interact with the web page.
     :return: A list of filtered hours in HH:MM format.
     """
-    raw_hours = extract_list(driver.get_elements_or_none_by_selector(selectors['hour']))
+    raw_hours = extract_list(
+        driver.get_elements_or_none_by_selector(selectors["hour"])
+    )
 
     # Regex to find times in HH:MM format
     time_pattern = re.compile("\d{2}:\d{2}")
     # Extract the schedules and form a list
-    hours = time_pattern.findall(' '.join(raw_hours))
+    hours = time_pattern.findall(" ".join(raw_hours))
 
     # Create a new list to keep filtered hours
     filtered_hours = []
@@ -79,24 +85,27 @@ def get_reactions(driver: AntiDetectDriver) -> list[tuple[list[str], int]]:
              - A list of emojis found in the reaction string.
              - The total number of reactions.
     """
-    raw_elements = driver.get_elements_or_none_by_selector(selectors['reactions'])
-    raw_reactions = [element.get_attribute('aria-label') for element in raw_elements]
+    raw_elements = driver.get_elements_or_none_by_selector(selectors["reactions"])
+    raw_reactions = [element.get_attribute("aria-label") for element in raw_elements]
 
     # region Extract emoji and total reactions
     # Pattern for emojis
-    emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"
-                               u"\U0001F300-\U0001F5FF"
-                               u"\U0001F680-\U0001F6FF"
-                               u"\U0001F1E0-\U0001F1FF"
-                               u"\U00002702-\U000027B0"
-                               u"\U000024C2-\U0001F251"
-                               u"\U0001f926-\U0001f937"
-                               u"\U0001F1F2"
-                               u"\U0001F1F4"
-                               u"\U0001F620"
-                               u"\U0001F916"
-                               "]+", flags=re.UNICODE)
+    emoji_pattern = re.compile(
+        "["
+        "\U0001F600-\U0001F64F"
+        "\U0001F300-\U0001F5FF"
+        "\U0001F680-\U0001F6FF"
+        "\U0001F1E0-\U0001F1FF"
+        "\U00002702-\U000027B0"
+        "\U000024C2-\U0001F251"
+        "\U0001f926-\U0001f937"
+        "\U0001F1F2"
+        "\U0001F1F4"
+        "\U0001F620"
+        "\U0001F916"
+        "]+",
+        flags=re.UNICODE,
+    )
 
     # Pattern for numbers, including possible dot or comma
     number_pattern = re.compile("[\d.,]+")
@@ -118,4 +127,6 @@ def get_reactions(driver: AntiDetectDriver) -> list[tuple[list[str], int]]:
     # endregion
 
     return result
+
+
 # endregion
