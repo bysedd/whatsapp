1a9bc4e160e276e506cf9b24d0e0c6a3056a8f9b
Felippe Andrade <4drade@gmail.com>
2024-01-08 13:57:08 -0300

    Refactored utils.py to use selectors imported from the constant module.

--- src/utils.py
+++ src/utils.py
@@ -1,27 +1,7 @@
 import re
-from string import Template
+from src.constants import selectors
 
 from botasaurus import AntiDetectDriver
-
-# region Selectors
-channel_template = Template(
-    "span[class='ggj6brxn gfz4du6o r7fjleex g0rxnol2 lhj4utae"
-    " le5p0ye3 l7jjieqr _11JPr'][title='$channel']"
-)
-
-selectors = {
-    "channels_button": "div[class='_3ndVb fbgy3m38 ft2m32mm oq31bsqd nu34rnf1']"
-    "[title='Canais']",
-    "channels": {
-        "tv_globo": channel_template.substitute(channel="TV Globo"),
-        "g1": channel_template.substitute(channel="g1"),
-    },
-    "message": "span[class='_11JPr selectable-text copyable-text']",
-    "hour": "span[class='l7jjieqr fewfhwl7'][dir='auto']",
-    "reactions": "button[class='dhq51u3o']",
-}
-
-# endregion
 
 
 # region Auxiliary functions
@@ -35,6 +15,14 @@ def extract_list(list_elements: list) -> list[str]:
     # Regex pattern to find consecutive whitespace
     pattern = re.compile(r"\s+")
     return [pattern.sub(" ", element.text.strip()) for element in list_elements]
+
+
+def valid_channel(channel: str) -> bool:
+    try:
+        return bool(selectors["channels"][channel])
+    except KeyError:
+        print(f"Invalid channel: '{channel}'")
+        return False
 
 
 def get_messages(driver: AntiDetectDriver) -> list[str]:
