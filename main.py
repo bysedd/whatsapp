87d5fc1267cc53467e17f33bc3ab8e0434ad8870
Felippe Andrade <4drade@gmail.com>
2024-01-08 14:42:02 -0300

    Update task function calls in the main module

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tasks.task import task
 
 if __name__ == "__main__":
-    task(channel='tv_globo')
-    task(channel='g1')
+    task(channel='tv_globo', headless=False)
+    task(channel='g1', headless=True)
