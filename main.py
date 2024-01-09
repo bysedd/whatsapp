dfa6230a97cac74d812943548df9f26b72eb8187
Felippe <4drade@gmail.com>
2024-01-09 16:18:32 -0300

    Import AVAILABLE_CHANNELS from constants
    The main.py file has been updated to include an import statement for AVAILABLE_CHANNELS from src/constants.py. This update will improve the handling of the different channel options in the main_task function. Also, a comment has been added for 'globo_com' channel for future possible use.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tasks.task import main_task
+from src.constants import AVAILABLE_CHANNELS
 
 if __name__ == "__main__":
-    main_task(channels=["tv_globo", 'g1'], headless=False)
+    main_task(channels=AVAILABLE_CHANNELS, headless=False)
+    # main_task(channels=["globo_com"], headless=True)
