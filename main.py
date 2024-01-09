b9b2d582c94af2c56ce115b90fe571c2fd2af9a9
Felippe <4drade@gmail.com>
2024-01-09 17:32:09 -0300

    Remove commented-out line in main.py
    Removed a commented out line of code from main.py as it is not needed. Cleaning up the code base by doing away with old, unused sections helps promote readability and maintainability.

--- main.py
+++ main.py
@@ -3,4 +3,3 @@ from src.constants import AVAILABLE_CHANNELS
 
 if __name__ == "__main__":
     main_task(channels=AVAILABLE_CHANNELS, headless=False)
-    # main_task(channels=["globo_com"], headless=True)
