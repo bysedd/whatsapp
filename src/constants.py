aaf5ea1fc5ee3a3e233873103ffff5da00a83e6f
Felippe Andrade <4drade@gmail.com>
2024-01-11 18:29:12 -0300

    Refactor constants.py with type annotations
    Enhanced the code readability in constants.py by addition of type annotations for global variables. Introduced a new constant, MINIMUM_MESSAGE_SIZE, to provide a threshold for data correlation. The changes aim to provide stricter type enforcement, which can help to improve code stability and maintainability.

--- src/constants.py
+++ src/constants.py
@@ -1,11 +1,11 @@
 from string import Template
 
-CHANNEL_TEMPLATE = Template(
+CHANNEL_TEMPLATE: Template = Template(
     "span[class='ggj6brxn gfz4du6o r7fjleex g0rxnol2 lhj4utae"
     " le5p0ye3 l7jjieqr _11JPr'][title='$channel']"
 )
-FILENAME_TEMPLATE = Template("$channel.csv")
-SELECTORS = {
+FILENAME_TEMPLATE: Template = Template("$channel.csv")
+SELECTORS: dict[str, str] = {
     "channels_button": "div[class='_3ndVb fbgy3m38 ft2m32mm oq31bsqd nu34rnf1']"
     "[title='Canais']",
     "channel": "span[class='ggj6brxn gfz4du6o r7fjleex g0rxnol2 lhj4utae le5p0ye3 "
@@ -15,6 +15,7 @@ SELECTORS = {
     "hour": "span[class='l7jjieqr fewfhwl7'][dir='auto']",
     "reactions": "button[class='dhq51u3o']",
 }
-WA_URL = "https://web.whatsapp.com/"
-LONG_TIME = 300
-SHORT_TIME = 10
+WA_URL: str = "https://web.whatsapp.com/"
+MINIMUM_MESSAGE_SIZE: int = 20
+LONG_TIME: int = 300
+SHORT_TIME: int = 10
