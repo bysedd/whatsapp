ff6e1d5e8b29d315480add67f11f2a4e405180f8
Felippe Andrade <4drade@gmail.com>
2024-01-11 08:20:35 -0300

    Remove unnecessary import and usage from main.py
    The unnecessary import of AVAILABLE_CHANNELS from src.constants has been removed from main.py. The main_task function call has been updated accordingly to reflect this change. This results in a simplified channel selection process and improves the flexibility of the code.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from src.constants import AVAILABLE_CHANNELS
 from tasks.task import main_task
 
 if __name__ == "__main__":
-    main_task(channels=AVAILABLE_CHANNELS, headless=False)
+    main_task(headless=False)
