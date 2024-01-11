57338492a49f58cc8b8dd746d909cb12617fb879
Felippe Andrade <4drade@gmail.com>
2024-01-11 08:20:19 -0300

    Simplify channel selectors in constants.py
    Channel selectors in constants.py have been revised to simplify the selection process. The channel-specific selectors have been replaced with more generic ones, improving the flexibility of channel selection. Additionally, the AVAILABLE_CHANNELS list derivation has been removed.

--- src/constants.py
+++ src/constants.py
@@ -8,18 +8,13 @@ FILENAME_TEMPLATE = Template("$channel.csv")
 SELECTORS = {
     "channels_button": "div[class='_3ndVb fbgy3m38 ft2m32mm oq31bsqd nu34rnf1']"
     "[title='Canais']",
-    "channels": {
-        "tv_globo": CHANNEL_TEMPLATE.substitute(channel="TV Globo"),
-        "globo": CHANNEL_TEMPLATE.substitute(
-            channel="globo.com I Últimas notícias: "
-            "jornalismo, esporte e entretenimento"
-        ),
-    },
+    "channel": "span[class='ggj6brxn gfz4du6o r7fjleex g0rxnol2 lhj4utae le5p0ye3 "
+               "l7jjieqr _11JPr']",
+    "channel_list": "div[class='_3YS_f _2A1R8'][aria-label='Lista de canais']",
     "message": "span[class='_11JPr selectable-text copyable-text']",
     "hour": "span[class='l7jjieqr fewfhwl7'][dir='auto']",
     "reactions": "button[class='dhq51u3o']",
 }
-AVAILABLE_CHANNELS = list(SELECTORS["channels"].keys())
 WA_URL = "https://web.whatsapp.com/"
 LONG_TIME = 300
 SHORT_TIME = 10
