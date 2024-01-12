f2075c77f7cb9c968769f1f416c6286775563ea8
Felippe Andrade <4drade@gmail.com>
2024-01-11 23:33:34 -0300

    Refactor and optimize utils.py
    Refactored and optimized utils.py code to use constant patterns, introduced a function to adjust list sizes, and simplified the align_message_data function. It significantly reduces the code complexity and enhances readability and maintainability.

--- src/utils.py
+++ src/utils.py
@@ -1,35 +1,38 @@
 import re
 
+WHITESPACE_PATTERN = re.compile(r"\s+")
+SPACE_PATTERN = re.compile(r"\s")
+SPLIT_PATTERN = re.compile(r"[.!]")
+
 
 def extract_list(list_elements: list) -> list[str]:
     """
     Extracts a list of strings from a given list of elements.
-
-    :param list_elements: A list of elements from which to extract strings.
-    :return: A list of strings extracted from the given list of elements.
     """
-    # Regex pattern to find consecutive whitespace
-    pattern = re.compile(r"\s+")
-    return [pattern.sub(" ", element.text.strip()) for element in list_elements]
+    return [WHITESPACE_PATTERN.sub(" ", element.text.strip()) for element in
+            list_elements]
 
 
-def align_elements(
-    messages: list[str], hours: list[str], reactions: list[tuple[list[str | None], int]]
+def adjust_list_length(data_list: list, target_length: int):
+    """
+    Ensure the list matches the target length by removing or adding elements as necessary.
+    """
+    while len(data_list) < target_length:
+        data_list.append(([], 0))
+    while len(data_list) > target_length:
+        data_list.pop(0)
+
+
+def align_message_data(
+        messages: list[str], hours: list[str],
+        reactions: list[tuple[list[str | None], int]]
 ):
     """
     Aligns the elements in the given lists.
-
-    :param messages: A list of strings representing messages.
-    :param hours: A list of strings representing hours.
-    :param reactions: A list of tuples consisting of a list of strings and an integer.
-    :return: A tuple of the aligned lists: messages, hours, and reactions.
     """
-    if len(reactions) > len(messages):
-        reactions.pop(0)
-    if len(reactions) < len(messages):
-        reactions.append(([], 0))
-    if len(hours) > len(messages):
-        hours.pop(0)
+    target_length = len(messages)
+    adjust_list_length(reactions, target_length)
+    adjust_list_length(hours, target_length)
 
     return messages, hours, reactions
 
@@ -38,9 +41,6 @@ def simplify_channel_name(channel_name: str) -> str:
     """
     Simplifies the channel name by replacing all spaces with underscores and
     splitting the name by period.
-
-    :param channel_name: The original channel name.
-    :return: The simplified channel name.
     """
-    name_without_spaces = re.sub(r"\s", "_", channel_name)
-    return re.split(r"[.!]", name_without_spaces)[0]
+    name_without_spaces = SPACE_PATTERN.sub("_", channel_name)
+    return SPLIT_PATTERN.split(name_without_spaces)[0]
