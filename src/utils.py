833249948d29e80fbcbb656f799c0209b1e27abb
Felippe Andrade <4drade@gmail.com>
2024-01-12 00:31:49 -0300

    Refactor utils.py and enhance data extraction
    Refactored utils.py by moving regular expression patterns to constants.py for better organization and readability. Improved the adjust_list_length function to return a list, and added a new function extract_data_to_dict that simplifies the process to extract data from messages, hours, and reactions and transform it into a dictionary format.

--- src/utils.py
+++ src/utils.py
@@ -1,26 +1,22 @@
-import re
-
-WHITESPACE_PATTERN = re.compile(r"\s+")
-SPACE_PATTERN = re.compile(r"\s")
-SPLIT_PATTERN = re.compile(r"[.!]")
+import src.constants as const
 
 
 def extract_list(list_elements: list) -> list[str]:
     """
     Extracts a list of strings from a given list of elements.
     """
-    return [WHITESPACE_PATTERN.sub(" ", element.text.strip()) for element in
+    return [const.WHITESPACE_PATTERN.sub(" ", element.text.strip()) for element in
             list_elements]
 
 
-def adjust_list_length(data_list: list, target_length: int):
+def adjust_list_length(data_list: list, target_length: int) -> list:
     """
-    Ensure the list matches the target length by removing or adding elements as necessary.
+    Ensure the list matches the target length by removing or adding elements as
+    necessary.
     """
-    while len(data_list) < target_length:
-        data_list.append(([], 0))
     while len(data_list) > target_length:
         data_list.pop(0)
+    return data_list
 
 
 def align_message_data(
@@ -30,9 +26,10 @@ def align_message_data(
     """
     Aligns the elements in the given lists.
     """
-    target_length = len(messages)
-    adjust_list_length(reactions, target_length)
-    adjust_list_length(hours, target_length)
+    target_length = min(len(messages), len(hours), len(reactions))
+    messages = adjust_list_length(messages, target_length)
+    hours = adjust_list_length(hours, target_length)
+    reactions = adjust_list_length(reactions, target_length)
 
     return messages, hours, reactions
 
@@ -42,5 +39,31 @@ def simplify_channel_name(channel_name: str) -> str:
     Simplifies the channel name by replacing all spaces with underscores and
     splitting the name by period.
     """
-    name_without_spaces = SPACE_PATTERN.sub("_", channel_name)
-    return SPLIT_PATTERN.split(name_without_spaces)[0]
+    name_without_spaces = const.SPACE_PATTERN.sub("_", channel_name)
+    return const.SPLIT_PATTERN.split(name_without_spaces)[0]
+
+
+def extract_data_to_dict(
+        messages: list[str],
+        hours: list[str],
+        reactions: list[tuple[list[str | None], int]]
+):
+    """
+    Extracts data from given parameters and returns a dictionary.
+
+    :param messages: list of messages
+    :param hours: list of hours
+    :param reactions: list of reactions
+    """
+    emojis, total = reactions
+    # Pad emojis with None if its length is less than 4
+    emojis = (emojis + [None] * 4)[:4]
+    return {
+        "message": messages,
+        "hour": hours,
+        "emoji_1": emojis[0],
+        "emoji_2": emojis[1],
+        "emoji_3": emojis[2],
+        "emoji_4": emojis[3],
+        "total": total,
+    }
