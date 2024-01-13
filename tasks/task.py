1c9621cd25f83e9057426297e3379208edc23e16
Felippe Andrade <4drade@gmail.com>
2024-01-13 20:06:18 -0300

    Improve data extraction by refactoring methods in task.py
    Direct references to the web driver have been removed from each extraction method, allowing passed parameters for greater flexibility. Class dependencies for the various extractor classes from the AbstractExtractor class have also been eliminated, streamlining the structure of the code.

--- tasks/task.py
+++ tasks/task.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 from botasaurus import *
 
 import src.constants as const
@@ -39,43 +37,45 @@ def main_task(*, headless: bool) -> None:
         """
         driver.get(const.WA_URL)
 
-        channel_extractor = task_utils.ChannelExtractor(driver)
+        channel_extractor = task_utils.ChannelExtractor()
         message_extractor = task_utils.MessageExtractor(driver)
         hour_extractor = task_utils.HourExtractor(driver)
-        reaction_extractor = task_utils.ReactionExtractor(driver)
+        reaction_extractor = task_utils.ReactionExtractor()
 
         # Wait 5 minutes for WhatsApp to open completely
         driver.click(const.SELECTORS["channels_button"], wait=const.LONG_TIME)
-        channels = channel_extractor.extract()
+        channels = channel_extractor.extract(driver=driver)
 
         for name, element in channels.items():
             channel_chat = driver.get_element_or_none_by_selector(
                 element, wait=const.LONG_TIME
             )
-            if channel_chat:
-                driver.click(element)
-                sleep(const.SHORT_TIME)
+            driver.click(element)
+            driver.sleep(const.SHORT_TIME)
+            utils.scroll_to_element(driver, const.SELECTORS["scroll_top"])
 
-                messages = message_extractor.extract()
-                hours = hour_extractor.extract()
-                reactions = reaction_extractor.extract()
+            post = utils.extract_list(driver.get_elements_or_none_by_selector(
+                const.SELECTORS["post"]
+            ))
 
-                messages, hours, reactions = utils.align_message_data(
-                    messages, hours, reactions
-                )
+            messages = message_extractor.extract(list_msgs=post)
+            hours = hour_extractor.extract(list_msgs=post)
+            reactions = reaction_extractor.extract(driver=driver)
 
-                # Create dataset with the obtained data
-                data = [
-                    utils.extract_data_to_dict(message, hour, reaction)
-                    for message, hour, reaction in zip(messages, hours, reactions)
-                ]
+            messages, hours, reactions = utils.align_message_data(
+                messages, hours, reactions
+            )
 
-                bt.write_csv(
-                    data[::-1],
-                    filename=const.FILENAME_TEMPLATE.substitute(channel=name),
-                )
+            # Create dataset with the obtained data
+            data = [
+                utils.extract_data_to_dict(message, hour, reaction)
+                for message, hour, reaction in zip(messages, hours, reactions)
+            ]
 
-                driver.refresh()
-                driver.click(const.SELECTORS["channels_button"], wait=const.LONG_TIME)
+            # Write a csv file for current channel
+            bt.write_csv(
+                data[::-1],
+                filename=const.FILENAME_TEMPLATE.substitute(channel=name),
+            )
 
     wrapper()
